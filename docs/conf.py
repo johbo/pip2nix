a10e2a1116635a2fc761f79bf3cf98ced22f31e3
Johannes Bornhold <johannes@bornhold.name>
2026-08-13 18:03:09 +0200

    build: Set the version to 0.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ author = 'Tomasz Kontusz'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '0.8.0.dev1'
+release = '0.10.0'
 # The short X.Y version.
 version = '.'.join(release.split('.')[:2])
 
